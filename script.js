e4226f142e08613f99c3597ed417b4caa0bc4212
mapingua.sw <mapingua.sw@gmail.com>
2025-09-22 10:25:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 const produtos = [
-  { nome: "OVERSIZED SUEDINE", preco: "GOLA RIBANA", tamanho: "", img: "imagens/detalhes01.png" },
-  { nome: "OVERSIZED SUEDINE", preco: "REFORÇO NO OMBRO", tamanho: "", img: "imagens/manga01.png" },
   { nome: "SNAKE | Branco (M)", preco: "SORTEADA!", tamanho: "M", img: "imagens/snake01.png" },
-  { nome: "HIGH | Preto (M)", preco: "R$ 74,99", tamanho: "M", img: "imagens/high01.png" },
-  { nome: "NEW BALANCE | Preto (M)", preco: "R$ 74,99", tamanho: "M", img: "imagens/newbalance01.png" },
-  { nome: "DGK SAKURA | Cinza (G)", preco: "R$ 74,99", tamanho: "G", img: "imagens/sakura01.png" },
-  { nome: "NIKE JORDAN AIR | Preto (G)", preco: "R$ 74,99", tamanho: "G", img: "imagens/nikejordan01.png" },
-  { nome: "SUPREME | Preto (G)", preco: "R$ 74,99", tamanho: "G", img: "imagens/supreme01.png" },
-  { nome: "OFF WHITE! | Bege (G)", preco: "R$ 74,99", tamanho: "G", img: "imagens/offwhite02.png" },
-  { nome: "JORDAN AIR | Preto (GG)", preco: "R$ 74,99", tamanho: "GG", img: "imagens/jordan01.png" },
-  { nome: "OFF WHITE! | Marrom (GG)", preco: "R$ 74,99", tamanho: "GG", img: "imagens/offwhite01.png" },
-  { nome: "ADIDAS | Azul Marinho (GG)", preco: "R$ 74,99", tamanho: "GG", img: "imagens/adidas01.png" },
+  { nome: "HIGH | Preto (M)", preco: "R$ 55,00", tamanho: "M", img: "imagens/high01.png" },
+  { nome: "NEW BALANCE | Preto (M)", preco: "R$ 55,00", tamanho: "M", img: "imagens/newbalance01.png" },
+  { nome: "DGK SAKURA | Cinza (G)", preco: "R$ 55,00", tamanho: "G", img: "imagens/sakura01.png" },
+  { nome: "NIKE JORDAN AIR | Preto (G)", preco: "R$ 55,00", tamanho: "G", img: "imagens/nikejordan01.png" },
+  { nome: "SUPREME | Preto (G)", preco: "R$ 55,00", tamanho: "G", img: "imagens/supreme01.png" },
+  { nome: "OFF WHITE! | Bege (G)", preco: "R$ 55,00", tamanho: "G", img: "imagens/offwhite02.png" },
+  { nome: "JORDAN AIR | Preto (GG)", preco: "R$ 55,00", tamanho: "GG", img: "imagens/jordan01.png" },
+  { nome: "OFF WHITE! | Marrom (GG)", preco: "R$ 55,00", tamanho: "GG", img: "imagens/offwhite01.png" },
+  { nome: "ADIDAS | Azul Marinho (GG)", preco: "R$ 55,00", tamanho: "GG", img: "imagens/adidas01.png" },
   { nome: "STÜSSY | Preto (GG)", preco: "ESGOTADA!", tamanho: "GG", img: "imagens/stussy01.png" }
 ];
 
@@ -51,6 +49,7 @@ document.getElementById("filtroTamanho").addEventListener("change", filtrar);
 
 // Carregar todos os produtos no início
 carregarProdutos(produtos);
+
 
 
 
