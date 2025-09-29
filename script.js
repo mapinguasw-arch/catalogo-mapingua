ee33768ad31d4a1c5acb202f08eb772ca08ecdad
mapingua.sw <mapingua.sw@gmail.com>
2025-09-29 15:21:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const produtos = [
   { nome: "SNAKE | Branco (M)", preco: "SORTEADA!", tamanho: "M", img: "imagens/snake01.png" },
   { nome: "HIGH | Preto (M)", preco: "R$ 55,00", tamanho: "M", img: "imagens/high01.png" },
-  { nome: "NEW BALANCE | Preto (M)", preco: "R$ 55,00", tamanho: "M", img: "imagens/newbalance01.png" },
+  { nome: "NEW BALANCE | Preto (M)", preco: "ESGOTADA!", tamanho: "M", img: "imagens/newbalance01.png" },
   { nome: "DGK SAKURA | Cinza (G)", preco: "ESGOTADA!", tamanho: "G", img: "imagens/sakura01.png" },
   { nome: "NIKE JORDAN AIR | Preto (G)", preco: "R$ 55,00", tamanho: "G", img: "imagens/nikejordan01.png" },
   { nome: "SUPREME | Preto (G)", preco: "R$ 55,00", tamanho: "G", img: "imagens/supreme01.png" },
@@ -49,6 +49,7 @@ document.getElementById("filtroTamanho").addEventListener("change", filtrar);
 
 // Carregar todos os produtos no início
 carregarProdutos(produtos);
+
 
 
 
